759f8bcecfe64feb440e960c3028d47f91489a1f
al7262 <alta5@alta5>
2019-12-13 01:26:50 +0700

    changed

--- store/urls.py
+++ store/urls.py
@@ -5,6 +5,5 @@ app_name = 'store'
 urlpatterns = [
     path('home', views.index),
     path('', views.slider),
-    path('base', views.base),
     path('header', views.header, name = 'header'),
 ]
--- store/views.py
+++ store/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'store/index.html', {})
-    
+    return render(request, 'store/base.html', {})
+
 def slider(request):
     return render(request, 'store/slider-banner.html', {})
 
