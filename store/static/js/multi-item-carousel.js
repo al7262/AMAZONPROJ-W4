c43aacdf4b80bfb05c3ac421e5a2983fd6104b71
al7262 <alta5@alta5>
2019-12-12 23:20:58 +0700

    commit

--- store/static/js/multi-item-carousel.js
+++ store/static/js/multi-item-carousel.js
@@ -3,17 +3,17 @@ $('#carouselExample').on('slide.bs.carousel', function (e) {
     var $e = $(e.relatedTarget);
     var idx = $e.index();
     var itemsPerSlide = 6;
-    var totalItems = $('.carousel-item').length;
+    var totalItems = $('.product .carousel-item').length;
     
     if (idx >= totalItems-(itemsPerSlide-1)) {
         var it = itemsPerSlide - (totalItems - idx);
         for (var i=0; i<it; i++) {
             // append slides to end
             if (e.direction=="left") {
-                $('.carousel-item').eq(i).appendTo('.carousel-inner');
+                $('.product .carousel-item').eq(i).appendTo('.product .carousel-inner');
             }
             else {
-                $('.carousel-item').eq(0).appendTo('.carousel-inner');
+                $('.product .carousel-item').eq(0).appendTo('.product .carousel-inner');
             }
         }
     }
