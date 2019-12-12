ebc47d15676276af0a33d8e96ee819f682f49a2c
al7262 <alta5@alta5>
2019-12-13 01:40:05 +0700

    combined index with footer and header

--- store/views.py
+++ store/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'store/base.html', {})
+    return render(request, 'store/index.html', {})
 
 def slider(request):
     return render(request, 'store/slider-banner.html', {})
