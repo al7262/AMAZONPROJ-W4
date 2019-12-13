af044c424dfb5e547bc73e0c826cfe69acc8303b
al7262 <alta5@alta5>
2019-12-13 08:30:59 +0700

    add file

--- store/views.py
+++ store/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
+from .models import User, Seller, Product, PaymentMethod, Order, Category, OrderDetail
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'store/index.html', {})
+    categories = Category.objects.all()
+    productList = []
+    for category in categories:
+        add = Product.objects.filter(category=category.id).all()
+        product.append(add)
+    return render(request, 'store/index.html', {
+        'categories': categories,
+        'listproduct': productList
+    })
 
 def slider(request):
     return render(request, 'store/slider-banner.html', {})
