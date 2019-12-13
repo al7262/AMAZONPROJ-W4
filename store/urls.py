cc05a2d973239b13a798236c846b7429e45fcb41
al7262 <alta5@alta5>
2019-12-13 14:05:33 +0700

    add file

--- store/urls.py
+++ store/urls.py
@@ -6,8 +6,6 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('product/<int:product_id>', views.product, name="product"),
     path('category/<int:category_id>', views.category, name="category"),
-    path('home', views.index),
-    path('', views.slider),
     path('header', views.header, name = 'header'),
     path('cart0', views.cart0, name = 'cart0'),
     path('cart1', views.cart1, name = 'cart1'),
